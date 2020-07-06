d7a1a74b6c5a616dde654ea1cef0a37de31119e8
changpw <1589273884@qq.com>
2020-07-06 21:33:09 +0800

    commit mybatis xml

--- src/main/java/com/cpw/sqlview/sqlview/Mappers/TablesMapper.java
+++ src/main/java/com/cpw/sqlview/sqlview/Mappers/TablesMapper.java
@@ -1,11 +1,9 @@
 package com.cpw.sqlview.sqlview.Mappers;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
-@Mapper
 public interface TablesMapper {
     @Select("select * from STM_SERIAL_NUMBER where SERIALNUMBER1 like '%${id}%' ")
     List<Map<String, Object>> getTableData(String id);
--- src/main/java/com/cpw/sqlview/sqlview/SqlviewApplication.java
+++ src/main/java/com/cpw/sqlview/sqlview/SqlviewApplication.java
@@ -1,8 +1,9 @@
 package com.cpw.sqlview.sqlview;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.cpw.sqlview.sqlview.Mappers")
 @SpringBootApplication
 public class SqlviewApplication {
 
